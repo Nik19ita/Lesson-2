6bb7fe28814cffa51ff6434a8904f0bad40fb432
Nikita <akter.kina@gmail.com>
2021-02-02 11:54:04 +0300

    add slider

--- Lesson 2/ViewController.swift
+++ Lesson 2/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControll: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: Segmented Controll
+        segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        // MARK: Label
+        mainLabel.text = "Выбери же что-нибудь :)"
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        
+        
+        //MARK: slider
+        
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
     }
 
 
+
+    @IBAction func actionSegmentedControll(_ sender: Any) {
+    
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Выбран первый сегмент"
+            mainLabel.backgroundColor = .black
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "Выбран второй сегмент"
+            mainLabel.backgroundColor = .white
+            mainLabel.textColor = .blue
+        case 2:
+            mainLabel.text = "Выбран третий сегмент"
+            mainLabel.backgroundColor = .yellow
+            mainLabel.textColor = .purple
+        default:
+            break
+        }
+    
+    }
 }
 
